843da30d55e6a617747cbd95e9229d75f57a4baa
Parvathy R <parvathy07r@gmail.com>
2024-06-16 16:06:04 +0530

    Added click functionality

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-/*
+const sections = document.querySelectorAll(".content");
+const makAllAsRead = document.querySelector(".btn")
+
+console.log(sections);
+sections.forEach(function(section) {
+    section.addEventListener('click', function(event) {
+        const status = document.querySelector(".status");
+        status.remove();
+    });
+});
+
+makAllAsRead.addEventListener('click', function(event) {
+    const statuses = document.querySelectorAll(".status");
+    statuses.forEach(function(status) {
+        status.remove();
+    })
+    });
 
 
-*/
